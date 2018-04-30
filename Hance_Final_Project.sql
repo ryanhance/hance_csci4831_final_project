21548c5a9a9f9732937eff734bc83d23a877ffdc
ryanhance <31717956+ryanhance@users.noreply.github.com>
2018-04-30 12:43:00 -0600

    Initial commit for project checkpoint #2
    Added checkpoint #2 SQL code, checkpoint #1 project explanation, and read me file with link to website at checkpoint #2.

---
+++ Hance_Final_Project.sql
@@ -0,0 +1,84 @@
+# Ryan Hance
+# 4-29-18
+# CSCI 4831
+# Final Project - Two-Out Performance %
+
+use retrosheet;
+
+# Find the number of times the batter scored the runner from 1B with 2 outs.
+# Events 4-13 are not events controlled by batter (i.e. balk, wild pitch, SB, etc)
+DROP TABLE IF EXISTS Score_Bat;
+CREATE TABLE Score_Bat as
+	select bat_id, count(*) as scoreBat from events e
+		where (e.EVENT_CD < 4 or e.EVENT_CD > 13) and bat_DEST_ID = 4 and outs_ct = 2
+		group by e.bat_id;
+
+# Find the number of times the batter scored the runner from 1B with 2 outs.
+# Events 4-13 are not events controlled by batter (i.e. balk, wild pitch, SB, etc)
+DROP TABLE IF EXISTS Score_1B;
+CREATE TABLE Score_1B as
+	select bat_id, count(*) as score1b from events e
+		where (e.EVENT_CD < 4 or e.EVENT_CD > 13) and run1_DEST_ID = 4 and outs_ct = 2
+		group by e.bat_id;
+    
+# Find the number of times the batter scored the runner from 2B with 2 outs.
+# Events 4-13 are not events controlled by batter (i.e. balk, wild pitch, SB, etc)
+DROP TABLE IF EXISTS Score_2B;
+CREATE TABLE Score_2B as
+	select bat_id, count(*) as score2b from events e
+		where (e.EVENT_CD < 4 or e.EVENT_CD > 13) and run2_DEST_ID = 4 and outs_ct = 2
+		group by e.bat_id;
+        
+# Find the number of times the batter scored the runner from 3B with 2 outs.
+# Events 4-13 are not events controlled by batter (i.e. balk, wild pitch, SB, etc)
+DROP TABLE IF EXISTS Score_3B;
+CREATE TABLE Score_3B as
+	select bat_id, count(*) as score3b from events e
+		where (e.EVENT_CD < 4 or e.EVENT_CD > 13) and run3_DEST_ID = 4 and outs_ct = 2
+		group by e.bat_id;
+        
+# Find the number of times the batter did not score a runner but got a BB, IBB, HBP, or hit.
+# Events 4-13 are not events controlled by batter (i.e. balk, wild pitch, SB, etc)
+DROP TABLE IF EXISTS NoScore;
+CREATE TABLE NoScore as
+	select bat_id, count(*) as noScore from events e
+		where ((e.EVENT_CD >= 14 and e.EVENT_CD <= 16) or e.EVENT_CD >= 20) 
+        and (BASE2_RUN_ID != "" or BASE3_RUN_ID != "") and outs_ct = 2 and EVENT_RUNS_CT = 0
+		group by e.bat_id;
+        
+# Find the number of times the batter had an opportunity to score runners in scoring position with two out.
+# Events 4-13 are not events controlled by batter (i.e. balk, wild pitch, SB, etc)
+DROP TABLE IF EXISTS Score_Opp;
+CREATE TABLE Score_Opp as
+	select bat_id, count(*) as scoreOpp from events e
+		where (e.EVENT_CD < 4 or e.EVENT_CD > 13) and (BASE2_RUN_ID != "" or BASE3_RUN_ID != "") and outs_ct = 2
+		group by e.bat_id;
+        
+# Find the number of times the batter got on base with two outs.
+# Events 4-13 are not events controlled by batter (i.e. balk, wild pitch, SB, etc)
+DROP TABLE IF EXISTS OnBase;
+CREATE TABLE OnBase as
+	select bat_id, count(*) as onBase from events e
+		where ((e.EVENT_CD >= 14 and e.EVENT_CD <= 16) or e.EVENT_CD >= 20) and outs_ct = 2
+		group by e.bat_id;
+        
+# Find the number of times the batter had a plate appearance with two outs.
+# Events 4-13 are not events controlled by batter (i.e. balk, wild pitch, SB, etc)
+DROP TABLE IF EXISTS PlateApp;
+CREATE TABLE PlateApp as
+	select bat_id, count(*) as plateApp from events e
+		where (e.EVENT_CD <= 3 or (e.EVENT_CD >= 14 and e.EVENT_CD <= 16) or e.EVENT_CD >= 18) and outs_ct = 2
+		group by e.bat_id;
+        
+# Join all the tables and calculate score
+select s1.bat_id, i.first, i.last, (1.5 * sb.scoreBat + 1.5 * s1.score1b + s2.score2b + s3.score3b 
+	+ .5 * ns.noScore) / opp.scoreOpp + ob.onBase / pa.plateApp as TOPP from Score_1B s1
+inner join Score_2B s2 on s1.bat_id = s2.bat_id
+inner join Score_3B s3 on s1.bat_id = s3.bat_id
+inner join Score_Bat sb on s1.bat_id = sb.bat_id
+inner join NoScore ns on s1.bat_id = ns.bat_id
+inner join Score_Opp opp on s1.bat_id = opp.bat_id
+inner join OnBase ob on s1.bat_id = ob.bat_id
+inner join PlateApp pa on s1.bat_id = pa.bat_id
+inner join id i on s1.bat_id = i.id
+order by TOPP desc;
---
+++ READ_ME.txt
@@ -0,0 +1,14 @@
+Ryan Hance
+4/30/18
+CSCI 4831
+Final Project - Two-Out Performance Percentage
+
+This is the final project repository for Ryan Hance's CSCI 4831 - Sabermetrics class for Spring 2018.
+
+Website:  https://sites.google.com/a/colorado.edu/hance-csci-4831-final-project/
+
+Contents include:
+- Read_Me.txt
+- Hance_Final_Project.sql - Code based used for this project
+- Hance_Final_Project.mp4 - Video explanation of project (Not yet added)
+- Hance_Final_Project.pdf - Project and statistic explanation and analysis
